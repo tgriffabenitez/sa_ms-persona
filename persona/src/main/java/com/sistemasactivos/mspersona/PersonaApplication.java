0a5092578ff9a05e4c91a8fc26924c626051dc76
tgriffabenitez <tgriffabenitez@gmail.com>
2023-04-13 09:25:07 -0300

    initial commit

---
+++ persona/src/main/java/com/sistemasactivos/mspersona/PersonaApplication.java
@@ -0,0 +1,13 @@
+package com.sistemasactivos.mspersona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonaApplication.class, args);
+    }
+
+}
