d76080c247d3f58d681b343670f9d83b5a59593b
tgriffabenitez <tgriffabenitez@gmail.com>
2023-06-07 15:43:37 -0300

    MModifico el metodo findByEmail, ahora devuelve un optional

--- ms-persona/src/main/java/com/sistemasactivos/mspersona/service/UserServiceImpl.java
+++ ms-persona/src/main/java/com/sistemasactivos/mspersona/service/UserServiceImpl.java
@@ -5,13 +5,15 @@ import com.sistemasactivos.mspersona.repository.BaseRepository;
 import com.sistemasactivos.mspersona.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserServiceImpl extends BaseServiceImpl<User, Long> {
     public UserServiceImpl(BaseRepository<User, Long> baseRepository) {
         super(baseRepository);
     }
 
-    public User findByEmail(String email) {
-        return ((UserRepository) baseRepository).findByEmail(email).orElse(null);
+    public Optional<User> findByEmail(String email) {
+        return ((UserRepository) baseRepository).findOneByEmail(email);
     }
 }
